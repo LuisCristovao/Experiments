04d809c215577a301734414f91b3c8b60d0fb96d
LuisCristovao <luis.cristovao92@gmail.com>
2019-01-16 21:56:11 +0000

    Update Blog Django

--- Blog/Django/applications/Django1/project1/clients/models.py
+++ Blog/Django/applications/Django1/project1/clients/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
+    # buzz word is field types
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
     salary = models.DecimalField(max_digits=5, decimal_places=2)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.first_name + " " + self.last_name)
