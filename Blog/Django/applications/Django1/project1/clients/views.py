83f8ed1465b3d365f86e73e8cd2c1b07cd05d201
LuisCristovao <luis.cristovao92@gmail.com>
2019-01-22 18:13:12 +0000

    new update on django blog

--- Blog/Django/applications/Django1/project1/clients/views.py
+++ Blog/Django/applications/Django1/project1/clients/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Person
+
 
 # Create your views here.
+def person_list(request):
+    persons = Person.objects.all()
+    return render(request, "list_persons.html", {'persons': persons})
+    # return HttpResponse("Bla bla it worked even though there is an error importing clients????")
--- Blog/Django/applications/Django1/project1/project1/urls.py
+++ Blog/Django/applications/Django1/project1/project1/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from clients import urls as clients_urls
 from django.contrib import admin
-from django.urls import path
-from .views import hello,person
-from django.urls import path
+from django.urls import path, include
+
+from .views import hello
 
 urlpatterns = [
     #path("hello/<str:name>", hello),
     path("hello/<str:name>", hello),
+    path('person/', include(clients_urls)),
     path('admin/', admin.site.urls),
-    path("person/<str:name>", person),
+    #path("person/<str:name>", person),
 
 ]
