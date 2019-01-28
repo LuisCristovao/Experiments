de4cf6146732211c7f0697a7dc079f74b00940ca
LuisCristovao <luis.cristovao92@gmail.com>
2019-01-28 18:52:09 +0000

    EscapeNightmare rocket fail
    Next try make with velocity

--- Unity/EscapeNightmare/Assets/Sripts/Player.cs
+++ Unity/EscapeNightmare/Assets/Sripts/Player.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+
+    public GameObject[] Thrusters;
+    public float rocket_force = 10;
+    public float force_increment = 0.1f;
+    public float min_force = 0;
+
+
     private Rigidbody rb;
     private Transform tr;
+    public bool flying_mode,on_air;
 	// Use this for initialization
 	void Start () {
+        flying_mode = false;
+        on_air = false;
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
 	}
@@ -14,28 +25,74 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey("a"))
+        if (!flying_mode)
         {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 2);
-            tr.rotation = Quaternion.AngleAxis(-90, new Vector3(0, 1, 0));
+            if (Input.GetKey("a"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 2);
+                tr.rotation = Quaternion.AngleAxis(-90, new Vector3(0, 1, 0));
+            }
+            if (Input.GetKey("d"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -2);
+                tr.rotation = Quaternion.AngleAxis(90, new Vector3(0, 1, 0));
+            }
+            if (Input.GetKey("w"))
+            {
+                rb.velocity = new Vector3(2, rb.velocity.y, rb.velocity.z);
+                tr.rotation = Quaternion.AngleAxis(0, new Vector3(0, 0, 0));
+            }
+            if (Input.GetKey("s"))
+            {
+                rb.velocity = new Vector3(-2, rb.velocity.y, rb.velocity.z);
+                tr.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            }
         }
-        if (Input.GetKey("d"))
+        else
         {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -2);
-            tr.rotation = Quaternion.AngleAxis(90, new Vector3(0, 1, 0));
+            
+            if (Input.GetKey("a"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 2);
+                tr.rotation = Quaternion.AngleAxis(-90, new Vector3(0, 1, 0));
+            }
+            if (Input.GetKey("d"))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -2);
+                tr.rotation = Quaternion.AngleAxis(90, new Vector3(0, 1, 0));
+            }
+            if (Input.GetKey("w"))
+            {
+                rb.velocity = new Vector3(2, rb.velocity.y, rb.velocity.z);
+                tr.rotation = Quaternion.AngleAxis(0, new Vector3(0, 0, 0));
+            }
+            if (Input.GetKey("s"))
+            {
+                rb.velocity = new Vector3(-2, rb.velocity.y, rb.velocity.z);
+                tr.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            }
+            
         }
-        if (Input.GetKey("w"))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector3(2, rb.velocity.y, rb.velocity.z);
-            tr.rotation = Quaternion.AngleAxis(0,new Vector3(0,0,0));
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.velocity = new Vector3(- 2, rb.velocity.y, rb.velocity.z);
-            tr.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            flying_mode = !flying_mode;
         }
         
 
+    }
 
+    
+
+    //on the air
+    private void OnCollisionExit(Collision collision)
+    {
+        on_air = true;
+    }
+    //touch ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        on_air = false;
     }
 }
---
+++ Unity/EscapeNightmare/Assets/Sripts/Rocket.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public float rocket_force=10;
+    public GameObject player;
+    public float rocket_increment = 0.1f;
+
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+
+
+    bool FlyingMode()
+    {
+        return player.GetComponent<Player>().flying_mode;
+    }
+    bool OnAir()
+    {
+        return !(player.transform.position.y < 3);
+        //return player.GetComponent<Player>().on_air;
+    }
+    
+
+    void RocketControl()
+    {
+       
+        if (!OnAir())
+        {
+            rocket_force += rocket_increment;
+        }
+        else
+        {
+            if (rocket_force > 0)
+            {
+                rocket_force -= rocket_increment;
+            }
+                
+        }
+        
+        
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (FlyingMode())
+        {
+            rb.AddForce(new Vector3(0, rocket_force, 0));
+            RocketControl();
+        }
+        
+	}
+}
