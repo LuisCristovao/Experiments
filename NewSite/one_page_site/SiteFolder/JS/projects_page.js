3d1e28d2dfb866db64c5e0f50c9cde0392b39ddd
LuisCristovao <luis.cristovao92@gmail.com>
2019-02-27 17:01:47 +0000

    Projects grid working!
    Next: put images same size

--- NewSite/one_page_site/SiteFolder/JS/projects_page.js
+++ NewSite/one_page_site/SiteFolder/JS/projects_page.js
@@ -1,3 +1,16 @@
+//Notes
+//<div class="col-lg-4 col-sm-6 portfolio-item" style="margin-bottom: 20pt">
+//          <div class="card " >
+//            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+//            <div class="card-body">
+//              <h4 class="card-title">
+//                <a href="#">Project One</a>
+//              </h4>
+//              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellatfscfse ewfwef ewf wef wefwe fwefwsef sdcv er fwefwfweff fwefweawdwad d w wqdq wdqwd qwd qwd qw qwdqwd asdcwdwd </p>
+//            </div>
+//          </div>
+//        </div>
+
 
 //Global functions
 async function getDBPosts() {
@@ -5,9 +18,28 @@ async function getDBPosts() {
     let val = await response.json();
     return val
 }
-async function show(){
-    data=await getDBPosts()
+async function fillGrid() {
+    var grid = document.getElementById("projects_grid")
+    data = await getDBPosts()
     alert(JSON.stringify(data))
+    var html = ""
+    for (index in data) {
+        var val = data[index]
+        html += '<div class="col-lg-4 col-sm-6 portfolio-item" style="margin-bottom: 20pt">'
+        html += '<div class="card " >'
+        html += '<a href="' + val["link"] + '"><img class="card-img-top" src="' + val["image url"] + '" alt=""></a>'
+        html += '<div class="card-body">'
+        html += '<h4 class="card-title">'
+        html += '<a href="' + val["link"] + '">' + val["title"] + '</a>'
+        html += '</h4>'
+        html += '<p class="card-text">' + val["short description"] + '</p>'
+        html += '</h4>'
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+
+    }
+    grid.innerHTML=html
 }
 //main-----
-show()
+fillGrid()
