12e5b516d7e2720ae4b4bec76c2683f6362be74d
LuisCristovao <luis.cristovao92@gmail.com>
2019-02-27 16:30:35 +0000

    projects page reads db

--- NewSite/one_page_site/SiteFolder/JS/projects_page.js
+++ NewSite/one_page_site/SiteFolder/JS/projects_page.js
@@ -1,1 +1,13 @@
-alert("project_page.js")
+
+//Global functions
+async function getDBPosts() {
+    let response = await fetch('SiteFolder/DB/all_posts.json');
+    let val = await response.json();
+    return val
+}
+async function show(){
+    data=await getDBPosts()
+    alert(JSON.stringify(data))
+}
+//main-----
+show()
