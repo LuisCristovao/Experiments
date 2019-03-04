7956e73f44d87ff78d125f70d2d66e321d92addf
LuisCristovao <luis.cristovao92@gmail.com>
2019-03-04 20:40:55 +0000

    fix bug with secondary tags

--- NewSite/one_page_site/SiteFolder/JS/SearchEngine.js
+++ NewSite/one_page_site/SiteFolder/JS/SearchEngine.js
@@ -19,7 +19,7 @@ class SearchEngine {
         for (var i in all_posts) {
             var post = all_posts[i]
             var post_tags = post['search tags'].split(",")
-            post_tags.concat(post['secondary search tags'].split(","))
+            post_tags=post_tags.concat(post['secondary search tags'].split(","))
             for (var j in post_tags) {
                 for (var e in query_tags) {
                     if (query_tags[e] == post_tags[j]) {
