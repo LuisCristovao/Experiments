53242dd99b5c0be66d74bf89eeba80b6e9f50f98
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2019-03-20 17:19:48 +0000

    contacts working!! (basic)

---
+++ Java/Spring/contacts/src/main/java/com/example/contacts/controller/ContactsController.java
@@ -0,0 +1,33 @@
+package com.example.contacts.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.contacts.model.Contacts;
+import com.example.contacts.repository.ContactsRepository;
+
+
+@RestController
+public class ContactsController {
+	
+	@Autowired
+    private ContactsRepository contactsRepository;
+
+    @GetMapping("/contacts")
+    public Page<Contacts> getQuestions(Pageable pageable) {
+        return contactsRepository.findAll(pageable);
+    }
+
+
+    @PostMapping("/contacts")
+    public Contacts createQuestion(@Valid @RequestBody Contacts question) {
+        return contactsRepository.save(question);
+    }
+}
---
+++ Java/Spring/contacts/src/main/java/com/example/contacts/model/Contacts.java
@@ -0,0 +1,55 @@
+package com.example.contacts.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "contacts")
+public class Contacts {
+	@Id
+    @GeneratedValue(generator = "contacts_generator")
+    @SequenceGenerator(
+            name = "contacts_generator",
+            sequenceName = "contacts_sequence",
+            initialValue = 1
+    )
+    private Long id;
+
+    @NotBlank
+    //@Size(min = 0, max = 100)
+    private String name;
+
+    //@Column(columnDefinition = "text")
+    @Column(name="number")
+    private String number;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+    
+}
