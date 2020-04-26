ef3d5e062c008f9e230dd579e216dcbba5e5bb2a
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2020-04-26 15:15:42 +0100

    export done

--- PasswordManager2/importPasswords.js
+++ PasswordManager2/importPasswords.js
@@ -1,24 +1,22 @@
-let password_import=getElement("password_import")
-let data=getElement("data")
-let split_data=getElement("spliting character")
-let columns=["site","user","pass","description"]
+let password_import = getElement("password_import")
+let data = getElement("data")
+let split_data = getElement("spliting character")
+let columns = ["site", "user", "pass", "description"]
 
 
-function Import(){
-    
-    let db=[]
-    let db_line={}
-    data.value.split('\n').forEach(row=>{
-        let columns_data=row.split(split_data.value)
-        columns_data.forEach((data,index)=>{
-            db_line[columns[index]]=encrypt(data,password_import.value)
-        })
+function Import() {
+
+    let db = []
+    let db_line = {}
+    data.value.split('\n').forEach(row => {
         
-        db.push(encrypt(JSON.stringify(db_line),password_import.value))
+        let columns_data = (split_data.value == '\\t')?row.split('\t'):row.split(split_data.value)
+        columns_data.forEach((data, index) => {
+            db_line[columns[index]] = encrypt(data, password_import.value)
+        })
+
+        db.push(encrypt(JSON.stringify(db_line), password_import.value))
     })
-    localStorage["PM"]=db.reduce((acc,n)=>acc+'\n'+n)
+    localStorage["PM"] = db.reduce((acc, n) => acc + '\n' + n)
+    data.value = "Imported passwords and encrypted them!"
 }
-
-
-
-
