f27da3b3e5c5cce903b7d85eba64cb311322ad55
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2020-05-24 11:39:10 +0100

    import old passwords

--- PasswordManager2/importPasswords.js
+++ PasswordManager2/importPasswords.js
@@ -17,6 +17,32 @@ function ImportDecryptedCSV(){
     html+=`<button onclick="Import()">Import</button>`
     getElement("importPasswords").innerHTML=html
 }
+function ImportFromOld(){
+    let html=getElement("importPasswords").innerHTML
+    html+=`<p>Insert text Passwords like |id|site|user|password| in textarea.</p>`
+    html+=`<p>Each line must be a new site password.</p><br>`
+    html+=`<p><b>Only use this option if you used application on...</b></p><br>`
+    html+=`Password use to encrypt:<input id="password_import" type="password"><input type="checkbox">show password<br><br>`
+    html+=`<textarea id="data"></textarea>`
+    html+=`<button onclick="oldImport()">Old Import</button>`
+    getElement("importPasswords").innerHTML=html
+}
+function oldImport(){
+    let password_import = getElement("password_import")
+    let data = getElement("data")
+    let filtered_data=""
+    data.value.split("\n").forEach(row => {
+        if(row==""){
+
+        }else{
+
+            let cols=row.split("\t")
+            filtered_data+=`${cols[1]}\t${cols[2]}\t${cols[3]}\tdescription...\n`
+        }
+    });
+    csvToDB(filtered_data,password_import.value,"\t")
+    data.value = "Imported passwords and encrypted them!"
+}
 function Import() {
 
     let password_import = getElement("password_import")
